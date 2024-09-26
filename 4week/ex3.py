d954ffee3a93331bb7a745aa9b7c65178fd55527
ksj <ksj97097@naver.com>
2024-09-26 12:51:57 +0900

    4주차 실습파일

--- 4week/ex3.py
+++ 4week/ex3.py
@@ -6,3 +6,14 @@ num2 = 20
 
 result = sum_int(num1, num2)
 print(f'{num1} + {num2} = {result}')
+
+def sumfunc(num):
+    sum = 0
+    for j in range(1,num+1):
+        sum = sum + j
+    return sum
+
+sum = sumfunc(num)
+print(f"1~{num}까지 정수의 합은 {sum} 입니다.")    
+
+
